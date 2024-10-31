fab196bc4030a465827606bb0946b6daee4857cd
Marcin Jeznach <Scheissig@gmail.com>
2024-10-31 04:00:28 +0100

    page release

--- src/components/Square.vue
+++
@@ -1,8 +0,0 @@
-<script setup lang="ts">
-</script>
-
-<template>
-	<p class="h-20 w-20 rounded-lg border border-white/70 ring-1 ring-black/30 dark:border-black/70 dark:ring-white/30 m-1 mx-8 flex items-center justify-center text-2xl">
-		<slot></slot>
-	</p>
-</template>
