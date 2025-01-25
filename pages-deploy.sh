4431de5f44d4c9288256bfe2ee9f37dc4e296b87
Marcin Jeznach <scheissig@gmail.com>
2025-01-25 15:27:36 +0100

    fixing deployment script is life/love

--- pages-deploy.sh
+++ pages-deploy.sh
@@ -2,39 +2,23 @@
 
 ## chmod +x pages-deploy.sh if it doesn't work ##
 
-# deploy project
 npm i
 npm run build
 
-# commit deployment
+git add dist/*
 git commit
 
-# Create a temporary directory
 temp_dir=$(mktemp -d)
-
-# Copy contents of dist directory to temporary directory
 cp -r dist/* "$temp_dir"
 
-# Switch to the pages branch
 git switch pages
-
-# Clear the current working directory
 rm -rf *
-
-# Copy the contents of the temporary directory to the current working directory
 cp -r "$temp_dir"/* .
 
-# Add all changes to git
 git add .
-
-# Commit with the default editor
 git commit
-
-# Push changes to the remote repository
 git push origin pages
 
-# Switch back to the main branch
 git switch main
 
-# Remove the temporary directory
 rm -rf "$temp_dir"
